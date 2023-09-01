bd39cae93c206ad2810c8af448852795ac901a27
mkiszkur <mkiszkur@gmail.com>
2023-09-01 20:25:05 -0300

    Corrida #9 | : 45170cp: 0| minsplit: 699| minbucket: 1 | maxdepth: 14

--- src/rpart/101_PrimerModelo.r
+++ src/rpart/101_PrimerModelo.r
@@ -18,6 +18,27 @@ dapply <- dataset[foto_mes == 202105] # defino donde voy a aplicar el modelo
 # genero el modelo,  aqui se construye el arbol
 # quiero predecir clase_ternaria a partir de el resto de las variables
 
+##Voy a generar variaciones pequenas del modelo reduciendo los datos. Saco el 3%
+# Define el porcentaje de datos que deseas retener
+
+cat("# filas de dtrain es:", nrow(dtrain), "\n")
+
+porcentaje_retencion <- 0.97
+
+# Calcula el número de filas que representan el 98% del dataset
+num_filas_retencion <- round(nrow(dtrain) * porcentaje_retencion)
+
+# Utiliza la función sample para seleccionar aleatoriamente las filas
+filas_seleccionadas <- sample(1:nrow(dtrain), num_filas_retencion)
+
+# Crea un nuevo dataframe con las filas seleccionadas
+dtrain <- dtrain[filas_seleccionadas, ]
+
+#Verifico cantidad de filas
+cat("# filas de dtrain es:", nrow(dtrain), "\n")
+
+
+
 #Volvemos al que mejor me fue
 # #Primer Intento
 modelo <- rpart(
